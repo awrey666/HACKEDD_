5d4ef40816a012a7c169e2f47a16fb63378ec0b1
AWREY <sharoyantelman000@mail.ru>
2025-03-09 13:24:42 +0400

    script.js

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const error = document.querySelector(".error")
 const succes = document.querySelector(".succes")
 
 
-
-
-
-
-
 if (localStorage.getItem("error") == "true") {
     error.classList.add("display_flex")
     warn.innerHTML = `У вас закончились попытки`
+}
+else if(localStorage.getItem("error") == "folse"){
+    succes.classList.add("display_flex")
+    warn.innerHTML = ``
 }
 else {
     let turn = 3
@@ -20,12 +19,15 @@ else {
     btn.addEventListener("click", function () {
         if (input.value !== "") {
             let x = input.value.trim()
+            input.value=""
             if (Number(x) !== 6 && turn !== 0) {
                 turn--
                 warn.innerHTML = `У вас осталось ${turn} попытки`
             }
             else if (Number(x) == 6) {
                 succes.classList.add("display_flex")
+                localStorage.setItem("error", folse)
+
             }
             if (turn == 0) {
                 error.classList.add("display_flex")
@@ -35,8 +37,3 @@ else {
         }
     })
 }
-
-
-
-
-
